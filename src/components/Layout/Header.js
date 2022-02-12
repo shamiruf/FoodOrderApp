0c2920a387c734f57debdf4f31d4ab321c14e1de
Rufi <43849574+shamiruf@users.noreply.github.com>
2022-02-12 18:50:20 +0100

    new components: cart, meals. mealItem etc.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 
 import styles from "./Header.module.css";
@@ -7,8 +8,10 @@ import styles from "./Header.module.css";
 const Header = (props) => {
   return (
     <>
-      <header className={styles.header}>GoodMeals</header>
-      <button>Cart</button>
+      <header className={styles.header}>
+        <h1>GoodMeals</h1>
+        <HeaderCartButton />
+      </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt={"Table with food"} />
       </div>
