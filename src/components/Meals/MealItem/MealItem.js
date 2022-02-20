147281f1c6dab8b46bbcff6d3979541c2c0b56cf
Rufi <43849574+shamiruf@users.noreply.github.com>
2022-02-20 22:03:45 +0100

    new component: Meal form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from "./MealItemForm";
 import styles from "./MealItem.module.css";
 
 const MealItem = (props) => {
@@ -9,7 +10,9 @@ const MealItem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
